f25b3c61409c7d92928fe5fa9fbd654e71d15f73
Prasoon Asati <prasouna@gmail.com>
2025-01-07 11:32:34 +0530

    add: preview page

--- src/app/campaign/create/generation/page.tsx
+++ src/app/campaign/create/generation/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { MAPPING_COLS } from "@/config/campaign";
 import { useToast } from "@/hooks/common/use-toast";
 import { useCamapignContext } from "@/providers/campaignProvider";
 import { CAMPAIGN_APIS } from "@/utils/apis";
@@ -9,6 +12,15 @@ const Generation = () => {
   const { id } = useCamapignContext();
   const { toast } = useToast();
   const [prospects, setProspects] = useState<any[]>([]);
+  const [selectedProspect, setSelectedProspect] = useState<string>("");
+
+  const handleSelect = (id: string) => {
+    if (selectedProspect == id) {
+      setSelectedProspect("");
+    } else {
+      setSelectedProspect(id);
+    }
+  };
 
   const fetchRegisteredProspects = async () => {
     try {
@@ -31,7 +43,51 @@ const Generation = () => {
       fetchRegisteredProspects();
     }
   }, [id]);
-  return <div>{JSON.stringify(prospects)}</div>;
+  return (
+    <div>
+      {" "}
+      <div className="grid gap-3 p-4">
+        <div className="flex items-end justify-between">
+          <div className="text-sm">
+            * please select any one to generate preview sample
+          </div>
+          <Button>Generate Preview</Button>
+        </div>
+        <table className="table-auto text-center w-full">
+          <thead className="bg-zinc-700">
+            <tr>
+              <td></td>
+              {MAPPING_COLS.map((col, index) => (
+                <td key={index} className="text-md px-2 py-1">
+                  {col.label}
+                </td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {prospects.map((prospect, index) => (
+              <tr key={prospect?._id || index}>
+                <td className="p-4 grid items-center ellispis">
+                  <Checkbox
+                    checked={prospect?._id == selectedProspect}
+                    onClick={() => handleSelect(prospect?._id!)}
+                  />
+                </td>
+                {MAPPING_COLS.map(({ value }, index) => (
+                  <td key={index} className="p-4 ellispis">
+                    {prospect[value] ?? "NA"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* <Button className="w-full" onClick={onSaveMapping} disabled={isLoading}>
+          Save Mapping
+        </Button> */}
+      </div>
+    </div>
+  );
 };
 
 export default Generation;
